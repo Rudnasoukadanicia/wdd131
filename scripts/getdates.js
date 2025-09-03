1ec3ccaa1ef9924732f7bfb4821995b84563fc02
rudnasouka <rsouka@byupathway.edu>
2025-09-03 15:01:12 +0100

    Core

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const currentYear = new Date().getFullYear();
     document.getElementById("currentYear").textContent = currentYear;
-
     const lastModified = document.lastModified;
     document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
 });
