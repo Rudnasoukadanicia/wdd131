8c16b9d1cef5661ed409acbb15c800a935576e82
rudnasouka <rsouka@byupathway.edu>
2025-09-03 12:48:17 +0100

    correctionII

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
     const currentYear = new Date().getFullYear(); document.getElementById("currentYear").textContent = currentYear; 
-    
     const lastModified = document.lastModified; document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`
 })
